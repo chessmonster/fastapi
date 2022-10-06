00c138e19f09014ecf4b3e7c84f645af93a2d2dd
Simon <arriolasimon@gmail.com>
2022-10-06 23:15:37 +0800

    minimize code

--- main.py
+++ main.py
@@ -4,28 +4,8 @@ import requests
 import json
 
 from fastapi import FastAPI
-from pydantic import BaseModel
-from typing import Optional
-
-class Item(BaseModel):
-    name: str
-    description: Optional[str] = None
-    price: float
-    tax: Optional[float] = None
 
 app = FastAPI()
-
-@app.post("/items/")
-async def create_item(item: Item):
-    item_dict = item.dict()
-    if item.tax:
-        price_with_tax = item.price + item.tax
-        item_dict.update({"price_with_tax": price_with_tax})
-    return item_dict
-
-@app.get("/hmm")
-async def hmm():
-    return {"hmmm":"hmmmmmm"}
 
 @app.get("/movie/{title}")
 async def movie(title):
