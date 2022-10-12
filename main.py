6d16bcc0bc397412057f97a5624afb7468c89f5e
Simon <arriolasimon@gmail.com>
2022-10-12 23:49:47 +0800

    add coirs

--- main.py
+++ main.py
@@ -4,8 +4,23 @@ import requests
 import json
 
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+origins = [
+    "http://localhost",
+    "http://localhost:8080",
+    # maybe add here the IP of amplify app
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/movie/{title}")
 async def movie(title):
